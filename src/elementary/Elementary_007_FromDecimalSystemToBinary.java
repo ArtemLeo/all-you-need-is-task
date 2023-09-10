dab704109afd6ccd592d23749255cfe0604e38ab
Artem Andriushchenko <link.by.artem@gmail.com>
2023-09-11 00:05:20 +0300

    add GeometricFigures_001_FloydTriangle

--- src/elementary/Elementary_007_FromDecimalSystemToBinary.java
+++ src/elementary/Elementary_007_FromDecimalSystemToBinary.java
@@ -2,18 +2,29 @@ package elementary;
 
 /*
   Convert the number 14 from decimal system to binary.
+
+   14 / 2 = 7 (remainder % 0)
+   7 / 2 = 3 (% 1)
+   3 / 2 = 1 (% 1)
+   1 / 2 = 0 (% 1)
+   ---------------
+   Write the results in reverse order: 1110.
  */
 
 public class Elementary_007_FromDecimalSystemToBinary {
     public static void main(String[] args) {
-        /*
-          14 / 2 = 7 (remainder % 0)
-           7 / 2 = 3 (% 1)
-           3 / 2 = 1 (% 1)
-           1 / 2 = 0 (% 1)
+        int decimalNumber = 14;
+        StringBuilder binaryNumber = new StringBuilder();
 
-           Write the results in reverse order: 1110.
-        */
+        if (decimalNumber == 0) {
+            binaryNumber = new StringBuilder("0");
+        } else {
+            while (decimalNumber > 0) {
+                int remainder = decimalNumber % 2;
+                binaryNumber.insert(0, remainder);
+                decimalNumber /= 2;
+            }
+        }
+        System.out.println("Binary representation of the number 14: " + binaryNumber);
     }
-
 }
--- src/elementary/Elementary_008_FromBinarySystemToDecimal.java
+++ src/elementary/Elementary_008_FromBinarySystemToDecimal.java
@@ -2,18 +2,26 @@ package elementary;
 
 /*
   Convert the number 1110 from the binary system to decimal.
+
+   0 * 2 ^ 0 = 0
+   1 * 2 ^ 1 = 2
+   1 * 2 ^ 2 = 4
+   1 * 2 ^ 3 = 8
+   -------------
+   0 + 2 + 4 + 8 = 14
  */
 
 public class Elementary_008_FromBinarySystemToDecimal {
     public static void main(String[] args) {
-        /*
-          0 * 2 ^ 0 = 0
-          1 * 2 ^ 1 = 2
-          1 * 2 ^ 2 = 4
-          1 * 2 ^ 3 = 8
+        String binaryNumber = "1110";
+        int decimalNumber = 0;
 
-          0 + 2 + 4 + 8 = 14
-        */
+        for (int i = binaryNumber.length() - 1, extent = 0; i >= 0; i--, extent++) {
+            char digit = binaryNumber.charAt(i);
+            int bitValue = Character.getNumericValue(digit);
+            int bitWeight = (int) Math.pow(2, extent);
+            decimalNumber += bitValue * bitWeight;
+        }
+        System.out.println("Decimal representation of the number 1110: " + decimalNumber);
     }
-
 }
