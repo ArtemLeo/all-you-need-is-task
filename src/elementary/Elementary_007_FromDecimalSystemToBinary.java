20f4d6a5884c873263a1179e1cf58f69289864c6
Artem Andriushchenko <link.by.artem@gmail.com>
2023-09-10 18:34:29 +0300

    add GeometricFigures_001_FloydTriangle

---
+++ src/elementary/Elementary_007_FromDecimalSystemToBinary.java
@@ -0,0 +1,19 @@
+package elementary;
+
+/*
+  Convert the number 14 from decimal system to binary.
+ */
+
+public class Elementary_007_FromDecimalSystemToBinary {
+    public static void main(String[] args) {
+        /*
+          14 / 2 = 7 (remainder % 0)
+           7 / 2 = 3 (% 1)
+           3 / 2 = 1 (% 1)
+           1 / 2 = 0 (% 1)
+
+           Write the results in reverse order: 1110.
+        */
+    }
+
+}
---
+++ src/elementary/Elementary_008_FromBinarySystemToDecimal.java
@@ -0,0 +1,19 @@
+package elementary;
+
+/*
+  Convert the number 1110 from the binary system to decimal.
+ */
+
+public class Elementary_008_FromBinarySystemToDecimal {
+    public static void main(String[] args) {
+        /*
+          0 * 2 ^ 0 = 0
+          1 * 2 ^ 1 = 2
+          1 * 2 ^ 2 = 4
+          1 * 2 ^ 3 = 8
+
+          0 + 2 + 4 + 8 = 14
+        */
+    }
+
+}
